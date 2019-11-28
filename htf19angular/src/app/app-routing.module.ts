1ff02f8a06f1e60afc9e16447decf448ec7fb7be
Fabio Puissant <fabio.puissant@hotmail.com>
2019-11-28 10:24:20 +0100

    material added

--- htf19angular/src/app/app-routing.module.ts
+++ htf19angular/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BankPageComponent } from './page/bank-page/bank-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ { path: 'banks', component: BankPageComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
