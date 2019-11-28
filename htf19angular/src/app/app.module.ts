730cb3ea8446e512432f42126b03c0529926e3ac
Fabio Puissant <fabio.puissant@hotmail.com>
2019-11-28 09:48:04 +0100

    Models aangemaakt

--- htf19angular/src/app/app.module.ts
+++ htf19angular/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BankCardComponent } from './bank-card/bank-card.component';
+
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
